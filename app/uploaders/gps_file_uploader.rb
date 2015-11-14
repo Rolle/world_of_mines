478565c18cd23a351522091c15377f984c2692b7
Roland <roland.schmitt@Web.de>
2015-11-14 21:03:52 +0100

    test

--- app/uploaders/gps_file_uploader.rb
+++ app/uploaders/gps_file_uploader.rb
@@ -9,7 +9,6 @@ class GpsFileUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-  process resize_to_fit: [1200, 1200]
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
